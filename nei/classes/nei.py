7a8a9e8507959700acb59161a97a8fe3d86a0e4e
Marcus Dupont <dupontdev@gmail.com>
2018-06-06 17:59:18 -0400

    Cleaned up line 1054

--- nei/classes/nei.py
+++ nei/classes/nei.py
@@ -1052,7 +1052,7 @@ class NEI:
                 new_n=self.hydrogen_number_density(new_time),
             )
 
-            =======
+
     def save(self, filename: str = "nei.h5"):
         """
         Save the `~nei.classes.NEI` instance to an HDF5 file.  Not
